0fd743dc942fe6d21807502823e5afa96ae7fa8c
James Desjardins <jadesjar@cisco.com>
2021-06-21 11:35:51 -0600

    add Cat Fact Component

---
+++ src/app/cat-fact/cat-fact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cat-fact',
+  templateUrl: './cat-fact.component.html',
+  styleUrls: ['./cat-fact.component.css']
+})
+export class CatFactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
